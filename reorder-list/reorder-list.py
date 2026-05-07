13f178bfde211927b0fcca25a1454245c4ddef78
__JOURA__ <zalishchuk.pn@ucu.edu.ua>
2026-05-07 19:12:46 +0300

    Create reorder-list.py

---
+++ reorder-list/reorder-list.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+
+        # Step 1: Find the middle of the linked list
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse the second half of the list
+        previous, current = None, slow
+        while current:
+            next_temp = current.next
+            current.next = previous
+            previous = current
+            current = next_temp
+
+        # Step 3: Merge the two halves
+        first, second = head, previous
+        while second.next:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+        
+        
